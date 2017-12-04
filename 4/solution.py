efc211a821e24cdb47c6964e520628e718c65b84
Timon Schmelzer <timon.schmelzer@tu-dortmund.de>
2017-12-04 10:16:58 +0100

    Add solution for riddle 4

---
+++ 4/solution.py
@@ -0,0 +1,28 @@
+def solve(file):
+    with open(file, 'r') as f:
+        n_valid = 0
+        for line in f.readlines():
+            line = line.split()
+            if len(set(line)) < len(line):
+                ### Invalid password
+                pass
+            else:
+                ### Valid password
+                n_valid += 1
+        return n_valid
+
+
+# Test examples
+files_and_solutions = {
+    'test1.txt': 1,
+    'test2.txt': 0,
+    'test3.txt': 1
+}
+
+for file, solution in files_and_solutions.items():
+    assert solve(file) == solution
+
+
+# Solve riddle
+print(solve('riddle.txt'))
+
