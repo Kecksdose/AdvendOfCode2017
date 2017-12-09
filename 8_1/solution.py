5653eb394fa61b69d8da4590fd242e6fcac25fb3
Timon Schmelzer <timon.schmelzer@tu-dortmund.de>
2017-12-09 19:25:23 +0100

    Add solution of 8 and 8_1

---
+++ 8_1/solution.py
@@ -0,0 +1,67 @@
+def check_condition(first, op, second):
+    if op == '>':
+        return first > second
+    elif op == '<':
+        return first < second
+    elif op == '==':
+        return first == second
+    elif op == '!=':
+        return first != second
+    elif op == '>=':
+        return first >= second
+    elif op == '<=':
+        return first <= second
+    else:
+        raise ValueError(f'Operation not known: {op}')
+
+def apply(val, op, number):
+    if op == 'inc':
+        return val + number
+    elif op == 'dec':
+        return val - number
+    else:
+        raise ValueError(f'Operation not known: {op}')
+
+def solve(file):
+    with open(file, 'r') as f:
+        all_vars = {}
+        all_values = set()
+
+        for line in f.readlines():
+            line = line.strip('\n')
+            line = line.split()
+
+            # Example line
+            # ['b', 'inc', '5', 'if', 'a', '>', '1']
+            #   0     1     2     3    4    5    6  <-- indices
+
+            # Initialise vars
+            for index in [0, 4]:
+                if not line[index] in all_vars.keys():
+                    all_vars[line[index]] = 0
+
+            # Check condition and apply operation
+            if check_condition(
+                first=all_vars[line[4]],
+                op=line[5],
+                second=int(line[6])
+            ):
+                all_vars[line[0]] = apply(
+                    val=all_vars[line[0]],
+                    op=line[1],
+                    number=int(line[2]))
+
+            all_values.add(all_vars[line[0]])
+            all_values.add(all_vars[line[4]])
+
+        return max(list(all_values))
+
+
+
+# Test examples
+assert solve('test.txt') == 10
+
+
+# Solve riddle
+print(solve('riddle.txt'))
+
